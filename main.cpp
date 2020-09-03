5d97372dc9979d7c43096ce8cbc177d84dbd59c3
eric <eric.thomas@formation-industries-fc.fr>
2020-09-03 16:32:36 +0200

    v2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "Produit.h"
+#include "Magasin.h"
 
 int main() {
-Produit p("pain",1,2);
-p.augmenterExemplaire(2);
-p.afficherDescription();
-p.editerDescription("Levain");
-cout<<p.getNom()<<" "<<p.getDescription()<<" "<<p.getNombreExemplaire()<<" "<<p.getPrixachat()<<" "<<p.getPrixvente()<<endl;
+    Produit p("pain", 1, 2);
+    p.augmenterExemplaire(2);
+    p.afficherDescription();
+    p.editerDescription("Levain");
+    cout << p.getNom() << " " << p.getDescription() << " " << p.getNombreExemplaire() << " " << p.getPrixachat() << " "
+         << p.getPrixvente() << endl;
+    Magasin m;
+    m.ajouterProduit("pain", 1, 2);
+    m.acheterProduit(0,2);
+    m.bilan();
+    m.vendreProduit(0,1);
+    m.bilan();
 }
