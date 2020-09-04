38b90f5202a6a16166dc1f57b0c5543b503c74e3
eric <eric.thomas@formation-industries-fc.fr>
2020-09-04 15:52:59 +0200

    with CD and Livrev+test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Produit.h"
 #include "Magasin.h"
 #include "Livre.h"
+#include "Cd.h"
 
 int main() {
 
@@ -14,5 +15,10 @@ int main() {
     Livre livre( "les vents", 10, 20, "Baudelaire", "Flammarion");
     livre.editerDescription("Roman");
     livre.afficherDescription();
+    vector<string> lespistes;
+    lespistes.push_back("Natacha");
+    lespistes.push_back("Le petit bonhomme en mousse");
+    Cd cd1("Les sardines",5,10,"Patrick Sebastien","Patrick Sebastien",lespistes);
+    cd1.afficherDescription();
 
 }
