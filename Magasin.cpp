e9063665a5941ea048e4f79fde509ea04eda1332
eric <eric.thomas@formation-industries-fc.fr>
2020-09-21 14:26:52 +0200

    final

--- Magasin.cpp
+++ Magasin.cpp
@@ -74,7 +74,6 @@ void Magasin::interaction() {
     cin>>saisie;
     switch(saisie)
     {
-
         case 1:
             this->submenu("produit");
             break;
@@ -103,63 +102,101 @@ void Magasin::submenu(const string &type) {
     cout<<"3-Afficher la description d'un "<<type<<"\n";
     cout<<"4-Ajouter un "<<type<<"\n";
     cout<<"5-Editer la description\n";
+    cout<<"6-Retour au menu principal\n";
     cin>>saisie;
-    switch(saisie)
-    {
+    switch(saisie) {
         case 1:
-            vendreProduit(this->choixproduit(type),this->choixqte());
-            break;
+            while(true) {
+                try {
+                    vendreProduit(this->choixproduit(type), this->choixqte());
+                    break; //casse la boucle si pas d'exception
+                }
+                    catch (logic_error & le)
+                    {
+                        cin.clear(); //remet cin en état ok
+                        cout << le.what() << endl;
+                    }
+                }
+        break; //break case
 
         case 2:
-            acheterProduit(this->choixproduit(type),this->choixqte());
+            while(true) {
+                try {
+                    acheterProduit(this->choixproduit(type), this->choixqte());
+                    break;
+                }
+                catch (logic_error &le) {
+                    cin.clear(); //remet cin en état ok
+                    cout << le.what() << endl;
+                }
+            }
             break;
-
         case 3:
             stock[choixproduit(type)]->afficherDescription();
             break;
 
         case 4:
-            if (type == "produit") {
-                while(true) {
-                    try{
+            while (true) {
+                try {
+                    if (type == "produit") {
+
+
                         this->ajouterProduit(this->saisieinformation("nom"), this->saisieprix("achat"),
                                              this->saisieprix("vente"));
-                        break;
-                    } catch (logic_error &le)
-                    {
-                        cout << le.what() << endl;
+
+                    }
+                 else if (type == "livre") {
+                    while (true) {
+                        this->ajouterlivre(saisieinformation("nom"), saisieprix("achat"), this->saisieprix("vente"),
+                                           this->saisieinformation("editeur"), this->saisieinformation("auteur"));
+
+                    }
+                } else {
+                    while (true) {
+                        this->ajouterCd(saisieinformation("nom"), saisieprix("achat"), this->saisieprix("vente"),
+                                        this->saisieinformation("auteur"), this->saisieinformation("editeur"),
+                                        this->saisielistetitre());
+
                     }
                 }
-            } else if (type == "livre") {
-                this->ajouterlivre(saisieinformation("nom"), saisieprix("achat"), this->saisieprix("vente"),
-                                   this->saisieinformation("editeur"), this->saisieinformation("auteur"));
-            } else {
-                this->ajouterCd(saisieinformation("nom"), saisieprix("achat"), this->saisieprix("vente"),
-                                this->saisieinformation("auteur"), this->saisieinformation("editeur"),
-                                this->saisielistetitre());
+            break;
+            } catch (logic_error & le)
+            {
+                cin.clear(); //remet cin en état ok
+                cout << le.what() << endl;
             }
+    }
 
             break;
         case 5:
             stock[choixproduit(type)]->editerDescription(this->saisieinformation("nouvelle description"));
             break;
 
+        case 6:
+            interaction();
+
 
 
     }
-    this->interaction();
+    submenu(type);
 }
 int Magasin::choixproduit(const string &type) {
     int saisie=0;
+    cin.ignore(256,'\n'); //ignore ce qui q été précedemment saisi
     cout<<"Veuillez donner le numéro du "<<type<<endl;
     cin>>saisie;
+    if (saisie<0||cin.fail())
+        throw logic_error("le numéro doit être positif ou égale à zéro et numérique");
     return saisie;
 }
 
 int Magasin::choixqte() {
     int saisie=0;
+    cin.ignore(256,'\n'); //ignore ce qui q été précedemment saisi
     cout<<"Quelle quantité"<<endl;
     cin>>saisie;
+    if (saisie<= 0||cin.fail())
+        throw logic_error("le quantité doit être au minimum de 1 et numérique");
     return saisie;
 }
 
@@ -176,11 +213,11 @@ const string Magasin::saisieinformation(const string &type) {
 float Magasin::saisieprix(const string &type)
 {
     float prix=0;
-
+    cin.ignore(256,'\n'); //ignore ce qui q été précedemment saisi
     cout << "Veuillez saisir prix " << type << "\n";
     cin >> prix;
-    if (prix < 0)
-        throw logic_error("le prix doit être positif");
+    if (prix < 0||cin.fail())
+        throw logic_error("le prix doit être positif et numérique");
     return prix;
 }
 
